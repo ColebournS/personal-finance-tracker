92bcc418f1d780a48122a4ef9fa5fb667cf38fd6
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2026-04-07 14:16:43 -0400

    Updates

---
+++ static/js/198.63b2a3a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_finance_tracker=self.webpackChunkpersonal_finance_tracker||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.63b2a3a9.chunk.js.map
